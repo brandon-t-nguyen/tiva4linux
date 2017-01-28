47af7ae51c328c640414c2882d30df92759aac94
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-01-28 00:19:01 -0600

    Added check for ARM toolchain

--- lm4linux.sh
+++ lm4linux.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-install_dir="$HOME/embedded-test"
+install_dir="$HOME/embedded"
 tivaware_dir="$install_dir/tivaware"
 lm4tools_dir="$install_dir/lm4tools"
 script_dir=$(pwd)
+
+
+if ! which arm-none-eabi-gcc > /dev/null;
+then
+    echo "ERROR: ARM toolchain is not setup properly. Are you sure you downloaded the ARM toolchain?"
+    exit 1
+fi
 
 if [ ! -e ./tivaware.exe ]
 then
@@ -25,3 +32,5 @@ cd $install_dir
 git clone git://github.com/aeturnus/tiva-template.git
 
 cd $script_dir
+
+echo "You will want to add the lm4flash to your PATH. It's located at $lm4tools_dir/lm4flash"
